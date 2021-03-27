c90d52b5287a467be8b61705f90fbcc5235f5268
vain05 <truongduy20226@gmail.com>
2021-03-27 23:25:00 +0700

    Update Main.cpp

--- FinalExamProject/Main.cpp
+++ FinalExamProject/Main.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world";
+	cout << "Minh nhan tin trong group it tuong tac qua, nen gui vai loi tam su o day\n"
+		<< "Mn co ranh sang thu 7 sau khi hoc xong cs moi tuan kh aaaa!!. De thong nhat hop nua huhuuuuu!!";
+
+	return 0;
 }
