b60d930d9aed5208e6c78fcd2bb2dc5519c4a421
Boorgar <hbdcat20@apcs.vn>
2021-03-30 08:48:58 +0700

    Change 1.0

--- FinalExamProject/Main.cpp
+++ FinalExamProject/Main.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Minh nhan tin trong group it tuong tac qua, nen gui vai loi tam su o day\n"
-		<< "Mn co ranh sang thu 7 sau khi hoc xong cs moi tuan kh aaaa!!. De thong nhat hop nua huhuuuuu!!";
+	cout << "Waifu for Laifu";
 
 	return 0;
 }
