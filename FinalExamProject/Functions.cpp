ed38ed9218e61d9db8e590d0f3ad1d195a01ce7f
Boorgar <hbdcat20@apcs.vn>
2021-04-04 20:39:56 +0700

    Changes

--- FinalExamProject/Functions.cpp
+++ FinalExamProject/Functions.cpp
@@ -1,6 +1,13 @@
 #include "Header.h"
 
-void readClassCSV(ifstream& classList, )
+void readClassCSV(ifstream& classList, Student* pHead) // Assume storing student as doubly linked list
 {
-
+	while (!classList.eof())
+	{
+		if (pHead == nullptr)
+		{
+			
+		}
+		getline(classList, )
+	}
 }
--- FinalExamProject/Header.h
+++ FinalExamProject/Header.h
@@ -1,8 +1,44 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
+
+struct schoolYear 
+{
+    int yearStart;
+    int yearEnd;
+    int semester;
+};
+
+struct Course
+{
+    schoolYear t;
+    string courseName;
+    string Id;
+    string teacher_name;
+    int numCredits;
+    int maxStudents;
+    string days;
+    int* sessions = new int[2]; // remember to delete
+};
+
+struct Staff {
+    string name;
+    string password;
+};
+
+struct Student 
+{
+    int ID;
+    string password;
+    int no;
+    string name;
+    bool gender;
+    string dateOfBirth;
+    string idSocial;
+};
 
 struct Node
 {
