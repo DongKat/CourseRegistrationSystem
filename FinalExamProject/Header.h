25dfe2acc89931a98735bf5f7b2deb4b7b5ad2bf
nhatanh2212 <tnanh20@apcs.vn>
2021-04-06 13:29:10 +0700

    Update Header.h

--- FinalExamProject/Header.h
+++ FinalExamProject/Header.h
@@ -9,51 +9,113 @@
 #include <vector>
 #include <sstream>
 
-
 using namespace std;
 
-struct schoolYear 
+struct date
+{
+    int day;
+    int month;
+    int year;
+};
+
+struct StaffAccount
+{
+    string name;
+    string password;
+    string address;
+    string phoneNumber;
+    date  dateOfBirth;
+};
+
+struct Schedules
+{
+    string day; // 8 lưu thứ MON, SAT,..
+    string time; // 8 Lưu ca S1, S2,..
+};
+struct Scores
+{
+    float GPA;
+    float Midterm;
+    float Final;
+    float Bonus;
+};
+
+struct CoursesStudent
+{
+    int sem;
+    date dateStart; // 7 ngày bắt đầu đăng ký môn
+    date dateEnd;  // 7 ngày kết thúc đăng ký môn
+    string courseName;
+    string courseID;
+    string teacher_name;
+    int numCredits;
+    int maxStudent;// cap phat studentID
+    int *studentID; //8 [50] là tối đa 50 SV 1 lớp - 13 ghi danh- 15 xoá khoá- 16 xem các môn mà sv học
+    Schedules *schedule[2];
+    Scores scoreBoard;// 22-26
+};
+
+struct CoursesSemester// 23 chi coi diem
+{
+    int sem;
+    date dateStart; // 7 ngày bắt đầu đăng ký môn
+    date dateEnd;  // 7 ngày kết thúc đăng ký môn
+    string courseName;
+    string courseID;
+    string teacher_name;
+    int numCredits;
+    int maxStudent;
+    CourseScore *mark;
+    
+};
+
+struct CourseScore
+{
+    string studentID;
+    Scores score;
+}
+
+struct Students
+{
+    int no;
+    int ID;
+    string password;
+    string name;
+    bool gender;
+    date dateOfBirth;
+    string idSocial;
+    Courses courseStudent[15];
+    // 13 [15] mỗi sem tối đa 5 course x3 sem, dùng tra cứu, kiểm tra thời gian buổi học - 14 xem danh sách các khoá đã ghi danh - 15 xoá khoá - 20-26
+    
+    Students *next;
+    Students *prev;
+};
+
+struct Semesters
+{
+    int sem;
+    date dateStart;// 6 ngày bắt đầu học kỳ
+    date dateEnd;// 6 ngày kết thúc học kỳ
+    Courses *course; // 7-11, 19 xem các môn có trong kỳ ( dù có học hay không học )
+};
+
+struct Classes
+{
+    string className;
+    Students *student; // 3 thêm sinh viên vào lớp , 18 xem danh sách sinh viên 1 lớp
+    Classes *next;
+    Classes *prev;
+};
+
+struct schoolYear // 1 tạo năm học
 {
     int yearStart;
     int yearEnd;
-    int semester;
+    Classes *Class; //2 tạo lớp - 17 xem danh sách lớp
+    Semesters Sem[3];
+    schoolYear *next;
+    schoolYear *prev;
 };
-
-struct Course
-{
-    schoolYear t;
-    string courseName;
-    string Id;
-    string teacher_name;
-    int numCredits;
-    int maxStudents;
-    string days;
-    int* sessions = new int[2]; // remember to delete
-};
-
-struct Staff {
-    string name;
-    string password;
-};
-
-struct Student 
-{
-    // No, Student ID, First name, Last name, Gender, Date of Birth, Social ID.
-    int ID;
-    string password = "a";
-    int no;
-    string firstName, lastName; // first nama, last name
-    string gender; // In case someone is Other gender, we are not homophobia here :)
-    string dateOfBirth;
-    string socialID;
-};
-
-struct Node
-{
-	Node* next, * prev;
-	int data;
-};
-
 
 void Nocursortype();
 void UnNocursortype();
