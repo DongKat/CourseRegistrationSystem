6c798d777772e970737f2fd1fbe1ddd475a42b8e
Boorgar <hbdcat20@apcs.vn>
2021-04-13 16:17:29 +0700

    Update Functions.h

--- FinalExamProject/Functions.h
+++ FinalExamProject/Functions.h
@@ -5,3 +5,16 @@
 
 void readClassCSV(ifstream& classList, Students& student);
 
+void createStudent(ifstream& classList, Students& student);
+
+void createStudentList(Students*& pHead, string csvFileName);
+
+void exportCourseStudent(Courses Course);
+
+void viewScoreboard(Courses Course);
+
+void importScoreboard(Courses Course);
+
+void updateStudentResult(Courses Course, string studentID);
+
+void viewClassScoreboard(Courses Course, string className);
