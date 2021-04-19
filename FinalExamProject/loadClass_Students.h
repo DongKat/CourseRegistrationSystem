b1723c3586bceedcebe178305e83dfc22ada7755
BLGCat <blgcat20@apcs.vn>
2021-04-19 19:43:38 +0700

    load classes, students
    Chưa xong nhe mọi người, lấy commit thôi

---
+++ FinalExamProject/loadClass_Students.h
@@ -0,0 +1,13 @@
+#ifndef _LOADCLASS_STUDENTS_H_
+#define _LOADcLASS_STUDENTS_H_
+
+#include "Struct.h"
+#include "Library.h"
+
+BasicCourses *loadCourse(string path);
+Students *loadStudentInfo(string path);
+Students *loadStudent(string path, string ClassName);
+Classes* loadClass();
+Classes* loadClass();
+
+#endif
