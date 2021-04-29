8025db0fd2f244219599fd646cfa92282f24d1f8
BLGCat <blgcat20@apcs.vn>
2021-04-29 12:08:18 +0700

    Update MenuStaff.cpp

--- FinalExamProject/MenuStaff.cpp
+++ FinalExamProject/MenuStaff.cpp
@@ -594,6 +594,17 @@ void MenuEditCourses(char username[], char password[], int sizeUser, int sizePas
 			gotoxy(140, 21); cout << " V I E W ";
 			if (choice == 13)
 			{
+				if (Sem == "") {
+					txtColor(15);
+					gotoxy(70, 20);
+					cout << "You must create semester first!";
+					Sleep(1000);
+					gotoxy(70, 20);
+					txtColor(0);
+					cout << "You must create semester first!";
+					continue;
+				}
+
 				txtColor(15);
 				
 				ifstream in;
